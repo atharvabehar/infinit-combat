55a4d564e426168e415287f50765fe35b34f8678
atharvabehar <69201688+atharvabehar@users.noreply.github.com>
2020-12-10 15:56:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,65 +1,96 @@
 var player , bullet ;
 var bg , ground;
-var ObstaclesGroup;
+var robotsGroup;
 var ammo = 0;
 var gun , gimg , pimg;
+var bg , b,sound ;
+var bgroup ;
+var gameState = "menu" ;
+var menu ,start ;
+
 function preload (){
-  gimg = loadImage ("download-removebg-preview (1).png")
-  pimg = loadImage ("images-removebg-preview.png")
-  r = loadImage("erob.png")
+    gimg = loadImage ("download-removebg-preview (1).png")
+    pimg = loadImage ("images-removebg-preview.png")
+    r = loadImage("erob.png")
+    bg = loadImage("lab1.jpg")
+    b = loadImage("bulet-removebg-preview.png")
+    sound = loadSound("gun reload.mp3")
+    menu = loadImage ("menu1.jpg")
 }
+
 function setup (){
-  createCanvas (displayWidth,displayHeight);
-  player = createSprite (200,height - 250 , 50,100);
-  player.addImage(pimg)
-  player.scale = 0.8
-  ground = createSprite (displayWidth/2 , height-10  , width , 10);
-  gun = createSprite (player.x+50,player.y-70,20,20)
-  gun.addImage(gimg)
-  gun.scale = 0.8
-  ground.visible = false;
-  ObstaclesGroup = new Group ();
-  player.debug = true
+    createCanvas (displayWidth,displayHeight);
+    player = createSprite (200,height - 250 , 50,100);
+    player.addImage(pimg)
+    player.scale = 0.8
+    ground = createSprite (displayWidth/2 , height-20  , width , 10);
+    gun = createSprite (player.x+50,player.y-70,20,20)
+    gun.addImage(gimg)
+    gun.scale = 0.8
+    ground.visible = false;
+    robotsGroup = new Group ();
+    player.setCollider("rectangle",0,0,player.width/2,player.height)
+    player.debug = true
+    start = createSprite(width/2, height/2 , 450,200)
+    start.visible = false
+    player.visible = false
+    gun.visible = false
 }
 function draw (){
-  background (0);
-  text("ammo :" + ammo,500,500)
-  edges = createEdgeSprites();
-  if (keyDown("space")){
-    player.velocityY = -12;
-    gun.velocityY = -12;
-  }
-  player.velocityY = player.velocityY + 0.8;
-  gun.velocityY = player.velocityY + 0.8;
-  player.y == gun.y
-  player.collide(ground);
-  gun.collide(ground);
-  spawnObstacles()
-  drawSprites();
-  
-}
-function spawnObstacles() {
-  if(World.frameCount % 60 === 0) {
-    var obstacle = createSprite(width,random(50,height-50),10,40);
-    obstacle.velocityX = - 20 //(6 + 3*count/100);
-    obstacle.addImage(r)
-    obstacle.scale = 0.7    
-   //generate random obstacles
-   // var rand = randomNumber(1,6);
-   // obstacle.setAnimation("obstacle" + rand);
+  if (gameState == "menu"){
+    background(menu)
     
-    //assign scale and lifetime to the obstacle           
-    //obstacle.scale = 0.5;
-    obstacle.lifetime = 5000;
-    //add each obstacle to the group
-    ObstaclesGroup.add(obstacle);
+    if(mousePressedOver(start)) {
+     gameState = "play"
+    }
+  }else if (gameState == "play"){
+
+    background (bg);
+    player.visible = true
+    gun.visible = true
+    text("ammo :" + ammo,500,500)
+    edges = createEdgeSprites();
+    if (keyDown("space")){
+      player.velocityY = -12;
+    
+    }
+    player.velocityY = player.velocityY + 0.8;
+    //gun.velocityY = player.velocityY + 0.8;
+    gun.y = player.y + 25
+    player.collide(ground);
+    gun.collide(ground);
+    spawnrobots()
+    if ( ammo <= 0 ){
+      textSize(55)
+      stroke ("red")
+      fill("red")
+      textFont("Georgia");
+      text("Reload!",width/2,height/2)
+    }
+    if (player.isTouching(robotsGroup)){
+  
+    }
+    
+  
+  }
+  drawSprites();
+ 
+}
+function spawnrobots() {
+  if(World.frameCount % 60 === 0) {
+      var robot = createSprite(width,random(50,height-50),10,40);
+      robot.velocityX = - 20 //(6 + 3*count/100);
+      robot.addImage(r)
+      robot.scale = 0.7    
+      robot.lifetime = 5000;
+      robotsGroup.add(robot);
   }
 }
-var x = document.getElementById("gun reload.mp3");function playAudio() {
+//var x = document.getElementById("gun reload.mp3");function playAudio() {
   
-}
+
 function playAudio() {
-  x.play();
+    sound.play();
 }
 
 function reloadammo(){
@@ -70,7 +101,31 @@ function reloadammo(){
 function keyPressed (){
   if ( keyCode == 82){
     playAudio()  
-    setTimeout(reloadammo,10000)
+    setTimeout(reloadammo,5000)
     
   }
 }
+
+function mouseClicked (){
+  if (ammo > 0){
+    ammo -= 1
+    var bullet = createSprite(400 , gun.y ,40 , 5);
+    bullet.addImage(b);
+    //bullet.collide(rightEdge);
+    bullet.velocityX = 10;       
+    bullet.lifetime = 100;
+    bullet.scale = 0.4
+  }
+    
+  
+}
+/*function istouching(o1,o2){
+  if (o1.x-o2.x < o1.width/2 + o2.width/2
+    && o2.x - o1.x < o1.width/2 + o2.width/2
+    && o1.y - o2.y < o1.height/2 + o2.height/2
+    && o2.y - o1.y < o1.height/2 + o2.height/2 ){
+      return  true;
+    }else {
+      return false
+    }
+    }*/
